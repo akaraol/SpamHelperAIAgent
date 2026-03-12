a2c77945562945c76d060b666a502e1881f4c7e7
akaraol <90260540+akaraol@users.noreply.github.com>
2026-03-13 02:40:13 +0300

    Emailmodel & Service Foundation

---
+++ Email-Cleaner-Agent/Models/EmailModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Email_Cleaner_Agent.Models;
+
+internal class EmailModel
+{
+    public Guid Id { get; set; }
+    public required string Sender { get; set; }
+    public required string Subject { get; set; }
+    public required string Body { get; set; }
+}
---
+++ Email-Cleaner-Agent/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Email_Cleaner_Agent.Services;
+
+internal interface IEmailService
+{
+}
